52da6971437f4e1a57652a2a3f19b20a98303ec1
victor1tnet <truica.victor.alexandru@gmail.com>
2014-01-30 14:24:07 +0100

    changed name

---
+++ ob-db-vars.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# db detailes
+DBUSER=''
+DBNAME=''
+
+# backup location detailes
+NASADDR='ftp://IP_ADDR'
+NASLOGIN='username:password'
+
+# script variables
+PDATE=$(date +"%m-%d-%y")
+# encryption key to be used with openssl
+ENCKEY=''
